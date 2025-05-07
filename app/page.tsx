b4f74a3f0b511b78119b752d38ac801436da7661
Violett <berezovski73@gmail.com>
2025-05-07 21:38:58 +0700

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,38 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className=''>
+			<div className='max-w-5xl mx-auto px-[0.9375rem] py-7 md:py-12 lg:py-24 pointer-events-none select-none'>
+				<p className='mb-4'>
+					Мне очень важно, чтобы ты знал: я действительно хочу общаться, но последнее время мне было
+					невероятно тяжело. Мне стыдно, что я отдалился, и я хочу объяснить, почему так вышло.
+				</p>
+				<p className='mb-4'>
+					На четвертом курсе колледжа я погрузился в депрессию. Это привело к отчислению, а потом
+					начались проблемы с работой в IT. Я чувствую, будто застрял: берусь за проекты, но бросаю
+					их, потому что теряю веру в себя. Из-за этого грусть и самобичевание стали постоянными
+					спутниками — даже с двоюродным братом перестал общаться, просто потому что стыдно за то,
+					каким я стал.
+				</p>
+				<div className='overflow-hidden'>
+					<Image
+						className='ml-[-2.1875rem] object-cover mb-4 pointer-events-none select-none'
+						width={200}
+						height={80}
+						src='/telegram.png'
+						alt='image'
+					/>
+				</div>
+				<p className='mb-4'>
+					Мне очень хочется вернуть наше общение, но сейчас даже простые разговоры даются с трудом.
+					Боюсь, что это выглядит так, будто мне всё равно, но это не так. Ты для меня важен, и я
+					надеюсь, что, когда станет чуть легче, мы сможем чаще говорить. Спасибо, что ты есть и что
+					не отворачиваешься.
+				</p>
+			</div>
+		</div>
+	)
 }
